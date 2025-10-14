9fe7059b26fd71ebb94a17b58010dde181faa7c6
Steven Lares <13951178+StevenLares@users.noreply.github.com>
2025-10-14 16:34:15 -0400

    Removed doc examples

--- lua/plugins/mappings.lua
+++ lua/plugins/mappings.lua
@@ -11,17 +11,6 @@ return {
           -- Remap macro recording to <Leader>0
           ["<Leader>0"] = { "q", desc = "Start/stop macro recording" },
 
-          -- Example buffer mappings from docs (optional)
-          ["<Leader>b"] = { desc = "Buffers" },
-          ["<Leader>bn"] = { "<cmd>tabnew<cr>", desc = "New tab" },
-          ["<Leader>bD"] = {
-            function()
-              require("astroui.status").heirline.buffer_picker(function(bufnr)
-                require("astrocore.buffer").close(bufnr)
-              end)
-            end,
-            desc = "Pick to close",
-          },
         },
       },
     },
