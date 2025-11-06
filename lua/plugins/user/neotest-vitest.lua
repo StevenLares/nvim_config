cf1671045a94c933907cb24cd9bca0eec2c58be1
Steven Lares <13951178+StevenLares@users.noreply.github.com>
2025-11-06 11:51:31 -0500

    Updated plugins to include testing

---
+++ lua/plugins/user/neotest-vitest.lua
@@ -0,0 +1,16 @@
+return {
+  "nvim-neotest/neotest",
+  dependencies = {
+    ...,
+    "marilari88/neotest-vitest",
+  },
+  config = function()
+    require("neotest").setup({
+      ...,
+      adapters = {
+        require("neotest-vitest"),
+      }
+    })
+  end,
+}
+
