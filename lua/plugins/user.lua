075ef232b47c0b82374af807567ce8284ec6408f
Steven Lares <13951178+StevenLares@users.noreply.github.com>
2026-06-20 17:20:55 -0400

    moved neotest-vitest to user.lua

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -5,6 +5,15 @@
 
 ---@type LazySpec
 return {
-
+  {
+    "nvim-neotest/neotest",
+    dependencies = {
+      "marilari88/neotest-vitest",
+    },
+    opts = function(_, opts)
+      opts.adapters = opts.adapters or {}
+      table.insert(opts.adapters, require("neotest-vitest"))
+    end,
+  },
 
 }
