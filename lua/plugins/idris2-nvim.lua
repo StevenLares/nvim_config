4e5795e4142e63de752eee0976ee22664b01961e
Steven Lares <13951178+StevenLares@users.noreply.github.com>
2026-06-20 15:28:50 -0400

    Fixed code actions for idris 2. Maybe this could be made more streamlined

--- lua/plugins/idris2-nvim.lua
+++ lua/plugins/idris2-nvim.lua
@@ -1,10 +1,28 @@
 return {
-  {
-    "idris-community/idris2-nvim",
-    lazy = true,
-    ft = { "idris2", "idr" }, -- filetypes to load on
-    config = function()
-      require("idris2").setup{} -- basic setup, extend as needed
-    end,
-  },
+        "idris-community/idris2-nvim",
+        dependencies = { 'neovim/nvim-lspconfig', 'MunifTanjim/nui.nvim' },
+
+        config = function()
+                local code_action = require('idris2.code_action')
+
+                local filters = code_action.filters
+                local introspect = code_action.introspect_filter
+                local function save_hook(action)
+                        -- https://github.com/idris-community/idris2-nvim/issues/33#issuecomment-2425178847
+                        if not action or not action.title then
+                                return
+                        end
+
+                        if introspect(action) == filters.MAKE_CASE
+                            or introspect(action) == filters.MAKE_WITH then
+                                return
+                        end
+                        vim.cmd('silent write')
+                end
+
+                require('idris2').setup({
+                        code_action_post_hook = save_hook,
+                })
+        end,
 }
+
